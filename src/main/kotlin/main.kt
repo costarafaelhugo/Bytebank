dac2a85c7687599429dc764f7c131297987e8eec
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-29 15:37:53 -0300

    Inserção e inicialização de variáveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
+
+    val titular:String = "Hugo Costa"
+
+    val numeroConta:Int = 1000
+
+    var saldo:Double = 100.0
+
+    println("Titular: $titular ")
+    println("Numero da conta: $numeroConta")
+    println("Saldo da conta: $saldo ")
 }
