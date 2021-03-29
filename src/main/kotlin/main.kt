b404238c33003c75380d3c3bbf4bbcc7f4e691f9
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-29 15:58:20 -0300

    Utilizando estruturas condicionais

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,30 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
 
-    val titular:String = "Hugo Costa"
+    val titular: String = "Hugo Costa"
 
-    val numeroConta:Int = 1000
+    val numeroConta: Int = 1000
 
-    var saldo:Double = 100.0
+    var saldo: Double = 100 + 2.0
+
+    saldo += 200
+    saldo = 0.0
+
+
 
     println("Titular: $titular ")
     println("Numero da conta: $numeroConta")
     println("Saldo da conta: $saldo ")
+
+    when {
+        saldo > 0.0 -> {
+            println("Conta é positiva")
+        }
+        saldo ==0.0 -> {
+            println("Conta é neutra")
+        }
+        else -> {
+            println("Conta é negativa")
+        }
+    }
 }
