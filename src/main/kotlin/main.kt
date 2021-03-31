9f9f36e141acf05fb5ebe0e5b56b11ac8074e7f0
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-31 09:54:32 -0300

    Implementando funcionario admin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,27 @@
 fun main() {
-    println("Bem vindo ao Bytebank!")
 
-   testaContasDiferentes()
+}
+
+fun testaAutenticacao() {
+    val gerente = Gerente(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0,
+        senha = 1000
+    )
+
+    val diretor = Diretor(
+        nome = "Fran",
+        cpf = "111.145.111-11",
+        salario = 2000.0,
+        senha = 2000,
+        plr = 200.0
+
+    )
+
+    val sistema = SistemaInterno()
+    sistema.entra(gerente, senha = 1000)
+    sistema.entra(gerente, senha = 2000)
 }
 
 
