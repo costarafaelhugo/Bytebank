9efb471056931d864a765d4f6c226f4e79950a72
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-31 08:26:43 -0300

    Implementando mebros abstaratos

---
+++ src/main/kotlin/Auxiliar.kt
@@ -0,0 +1,14 @@
+class Auxiliar(
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+
+    override val bonificacao: Double
+        get(){return  salario *.1
+        }
+}
